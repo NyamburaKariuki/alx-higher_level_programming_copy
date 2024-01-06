bc4ffeeaf64e4e2af0b33f169be44fbadebe29ef
NyamburaKariuki <prettimercii@gmail.com>
2024-01-06 09:00:04 +0300

    More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,6 +4,6 @@
 def multiple_returns(sentence):
     '''returns string length and first character'''
     if len(sentence) == 0:
-        return (None)
+        return (None, 0)
     else:
         return (len(sentence), sentence[0])
