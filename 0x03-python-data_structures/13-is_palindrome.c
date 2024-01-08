114227f05f1b62818020cf478d9c6d818d2856cc
NyamburaKariuki <prettimercii@gmail.com>
2024-01-08 22:27:40 +0300

    is pallidrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,30 @@
+#include <stddef.h>
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * is_pallidrome - check if a list is a pallidrome
+ * @head: pointer to pointer of first node of listint_t list
+ * Return: 1 (is pallidrome) or 0 (not pallidrome)
+ */
+int is_palindrome(listint_t **head)
+{
+int x;
+int n = 0;
+int num[254];
+listint_t *move;
+
+if (head == NULL)
+return (0);
+move = *head;
+while (move != NULL)
+{
+num[n++] = move->n;
+move = move->next;
+}
+for (x = 0; x < n / 2; x++)
+{
+if (num[x] != num[n - 1 - x])
+return (0);
+}
+return (1);
+}
