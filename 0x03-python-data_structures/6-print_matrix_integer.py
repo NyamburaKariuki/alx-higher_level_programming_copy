8f9810991c6096643c71d9051e68d10550805643
NyamburaKariuki <prettimercii@gmail.com>
2024-01-08 20:25:17 +0300

    More returns!

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,5 @@ def print_matrix_integer(matrix=[[]]):
     '''that prints a matrix of integers'''
     for x in range(len(matrix)):
         for y in range(len(matrix[x])):
-            print("{:d}".format(matrix[x][y]), end="")
-            if y != (len(matrix[x]) - 1):
-                print(" ", end="")
-        print(" ")
+            print("{:d}".format(matrix[x][y]), end=" ")
+        print()
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,6 +4,6 @@
 def multiple_returns(sentence):
     '''returns string length and first character'''
     if len(sentence) == 0:
-        return (None, 0)
+        return (0, None)
     else:
         return (len(sentence), sentence[0])
