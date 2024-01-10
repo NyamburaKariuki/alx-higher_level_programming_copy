22821dc69e017094cbef5b34789c3a658f578ca9
NyamburaKariuki <prettimercii@gmail.com>
2024-01-11 00:10:41 +0300

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,6 +3,6 @@
 
 def best_score(a_dictionary):
     ''' returns a key with the biggest integer value'''
-    if a_dictionary != None:
+    if a_dictionary:
         return max(a_dictionary, key=a_dictionary.get)
     return None
