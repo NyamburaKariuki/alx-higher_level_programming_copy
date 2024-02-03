14ba516c9309b02bb6a586e9f22cbc78e8f753dd
NyamburaKariuki <prettimercii@gmail.com>
2024-02-03 11:31:31 +0300

    TDD

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -9,7 +9,7 @@ def add_integer(a, b=98):
     returns the sum of the two"""
 
     if not isinstance(a, int) and not isinstance(a, float):
-        raise TypeError("a must be an integer or float")
+        raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
-        raise TypeError("b must be an integer of float")
-    return (int(a) + int(b))
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
