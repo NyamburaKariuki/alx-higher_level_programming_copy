3783695e57a769ded379dde383f2b32c50a26982
NyamburaKariuki <prettimercii@gmail.com>
2024-02-05 13:30:40 +0300

    fn returns the list of available attributes and methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""defines a function thats returns attributes and methods os an ojbect"""
+
+
+def lookup(obj):
+    """returns the list of available attributes and methods of an object"""
+    return dir(obj)
