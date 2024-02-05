48190bcac842a9a1e6f4f89dca90458922105350
NyamburaKariuki <prettimercii@gmail.com>
2024-02-06 00:48:30 +0300

    a class MyInt that inherits from int

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""defines a class MyInt that inherits from int"""
+
+
+class MyInt(int):
+    """inverts operators == and !="""
+
+    def __eq__(self, other):
+        """inverts operator == to !="""
+        return super(). __ne__(other)
+
+    def __ne__ne(self, other):
+        """inverts operator != to =="""
+        return super().__eq__(other)
