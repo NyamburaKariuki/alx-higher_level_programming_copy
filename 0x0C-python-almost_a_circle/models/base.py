5c333dd715978f6d197349d103be2a42b350357b
NyamburaKariuki <prettimercii@gmail.com>
2024-02-11 23:48:04 +0300

    class Base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -32,11 +32,19 @@ class Base:
         if not list_objs:
             pass
         else:
-            for i in range (len(list_objs)):
+            for i in range(len(list_objs)):
                 list_d.append(list_objs[i].to_dictionary())
         list_s = cls.to_json_string(list_d)
         with open(filename, 'w') as file:
             file.write(list_s)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """eturns the list of the JSON string representation json_string"""
+        if json_string is None or json_string == "[]":
+            return "[]"
+        else:
+            return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
@@ -48,3 +56,14 @@ class Base:
                 dummy = cls(1)
             dummy.update(**dictionary)
             return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """returns a list of instances"""
+        filename = "{}.json".format(cls.__name__)
+        try:
+            with open(filename, 'r') as f:
+                lists = Base.from_json_string(f.read())
+                return [cls.create(**dicts) for dicts in lists]
+        except IOError:
+            return []
