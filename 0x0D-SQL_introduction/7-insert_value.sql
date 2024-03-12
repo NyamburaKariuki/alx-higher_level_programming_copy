bef2004a17f7d79d92118c171736b972e3b9bf52
NyamburaKariuki <prettimercii@gmail.com>
2024-03-12 23:23:54 +0300

    count 89

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
 -- First add
 -- inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
 INSERT INTO first_table (id, name)
-VALUES (89, 'Best Schhol')
+VALUES (89, "Best Schhol")
