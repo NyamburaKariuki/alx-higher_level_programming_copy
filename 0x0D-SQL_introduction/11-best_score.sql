92a18d87ba51952d7595763e18ade314aab020f4
NyamburaKariuki <prettimercii@gmail.com>
2024-03-13 00:20:01 +0300

    11-best_score.sql

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,5 +1,5 @@
 -- select the best
--- lists all records with a score >= 10 in the table second_table of the database
+-- lists all records with a score >= 10 in the table second_table 
 SELECT score, name;
 FROM second_table;
 WHERE score >= 10;
