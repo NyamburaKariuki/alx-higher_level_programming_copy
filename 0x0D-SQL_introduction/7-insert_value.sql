4e1bff65c9d146b1e574dba3f9fdfa01d7898193
NyamburaKariuki <prettimercii@gmail.com>
2024-03-12 23:53:57 +0300

    7-insert_value.sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- First add
--- inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
+-- inserts a new row in the table first_table (database hbtn_0c_0) 
 INSERT INTO first_table (id, name) VALUES (89, "Best Schhol")
