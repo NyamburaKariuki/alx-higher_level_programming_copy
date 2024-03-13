a7c165ca217014d179bb8634f27c8c4a3a895694
NyamburaKariuki <prettimercii@gmail.com>
2024-03-13 18:21:13 +0300

    always a name

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,2 @@
 -- creates the table force_name with id INT and name VARCHAR(256) not Null
-CREATE TABLE (id INT, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF EXISTS (id INT, name VARCHAR(256) NOT NULL);
