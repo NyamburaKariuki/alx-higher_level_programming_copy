c0847c32eee1e34dffcc9b55d2ad7fc8438dbe95
NyamburaKariuki <prettimercii@gmail.com>
2024-03-13 21:56:45 +0300

    cities table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,4 +1,4 @@
--- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) 
+-- creates the database hbtn_0d_usa and the table cities in the database hbtn_0d_us) 
 -- id INT unique, auto generated, can’t be null and is a primary key
 -- state_id INT, can’t be null and must be a FOREIGN KEY that references to id of the states table
 -- name VARCHAR(256) can’t be null
