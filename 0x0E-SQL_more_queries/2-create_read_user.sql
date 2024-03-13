cbdc9bc0643b3d9d5bdb72aaf7fda76008462c8a
NyamburaKariuki <prettimercii@gmail.com>
2024-03-13 17:50:12 +0300

    read user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- creates a database hbtn_0d_2 and user user_0d_2 with select priveleges only
+-- user_0d_2 password is user_0d_2_pwd
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS user_0d_2 IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT hbtn_0d_2.* TO user_0d_2;
