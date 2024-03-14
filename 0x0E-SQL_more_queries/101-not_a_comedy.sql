a22614c372cdf9aa65970b362aba481f0b8fb1ef
NyamburaKariuki <prettimercii@gmail.com>
2024-03-14 13:21:49 +0300

    dumps

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,18 +1,11 @@
 -- Get the shows with the genre Comedy
-SELECT tv_shows.id
+SELECT title
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name = 'Comedy';
-
--- Get all shows without the genre Comedy
-SELECT tv_shows.title
-FROM tv_shows
-WHERE tv_shows.id NOT IN (
-    SELECT tv_shows.id
-    FROM tv_shows
-    LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-    LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-    WHERE tv_genres.name = 'Comedy'
-)
-ORDER BY tv_shows.title ASC;
+WHERE title NOT IN
+(SELECT title
+	FROM tv_shows
+	LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+	LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+	WHERE tv_genres.name = 'Comedy')
+GROUP BY title
+ORDER BY title ASC;
