ea5931399eb922271831c2828db6a26f03880eb2
NyamburaKariuki <prettimercii@gmail.com>
2024-03-14 13:19:58 +0300

    dumps

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,18 +1,11 @@
 -- Get the genres linked to the show Dexter
-SELECT tv_genres.id
+SELECT name
 FROM tv_genres
-INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title = 'Dexter';
-
--- Get all genres not linked to the show Dexter
-SELECT tv_genres.name
+WHERE name NOT IN
+(SELECT name
 FROM tv_genres
-WHERE tv_genres.id NOT IN (
-    SELECT tv_genres.id
-    FROM tv_genres
-    INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-    INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-    WHERE tv_shows.title = 'Dexter'
-)
-ORDER BY tv_genres.name ASC;
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter')
+GROUP BY name
+ORDER BY name ASC;
